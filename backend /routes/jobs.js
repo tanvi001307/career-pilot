e47fd66a5232ea9ec480a950788477c8fff3378d
tanvi001307 <tanvi.001307@gmail.com>
2026-01-01 21:10:58 +0530

    Update jobs.js

--- backend /routes/jobs.js
+++ backend /routes/jobs.js
@@ -1,19 +1,4 @@
-// import express from "express";
-// import { fetchJobs } from "../services/jobFetcher.js";
 
-// const router = express.Router();
-
-// router.get("/", async (req, res) => {
-//   try {
-//     const jobs = await fetchJobs();
-//     res.json(jobs);
-//   } catch (err) {
-//     res.status(500).json({ error: "Failed to fetch jobs" });
-//   }
-// });
-
-
-// export default router;
 
 import express from "express";
 import { fetchJobs } from "../services/jobFetcher.js";
